329a84b9083e9a69ecf6aa54aa977255348a7fb2
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2017-09-12 15:16:53 +0200

    add snakefile creation and restructure group file

--- dge_pipeline/dge.py
+++ dge_pipeline/dge.py
@@ -1,10 +1,21 @@
 import argparse
 import errno
 import os
+from collections import namedtuple
+
+from snakemake import snakemake
 
 from dge_pipeline.customErrors import InvalidGroupsFileError
 
 PROGRAM_NAME = "Differential gene expression pipeline"
+
+SNAKEFILES = {
+    "bowtie2": os.path.join(os.path.dirname(__file__), "snakefiles/dge_bowtie2_pe"),
+    "bwa": os.path.join(os.path.dirname(__file__), "snakefiles/dge_bwa_samse")
+}
+
+GroupEntry_PE = namedtuple('GroupEntry_PE', ['name', 'condition', 'forward', 'reverse'])
+GroupEntry_SE = namedtuple('GroupEntry_SE', ['name', 'condition', 'file'])
 
 
 def main():
@@ -12,19 +23,28 @@ def main():
     for arg in args.__dict__:
         print(f"{arg}: {args.__dict__[arg]}")
     validate_argsfiles(args.groups_file, args.ref_genome_file, args.ref_annotation_file)
-    groups = parse_groups_file(args.groups_file)
+    groups = parse_groups_file(args.groups_file, args.pe)
     validate_inputfiles(groups, args.pe)
+    create_output_directory(args.output_folder)
+    create_snakefile(SNAKEFILES["bowtie2"], args.output_folder)
 
 
-def parse_groups_file(groups_file):
+def parse_groups_file(groups_file, isPE):
     groups = []
     file = open(groups_file, 'r')
     for entry in file:
+        if len(entry.strip()) == 0:
+            continue
         cols = entry.strip().split('\t')
-        cols[0] = os.path.abspath(os.path.join(os.path.dirname(groups_file), cols[0]))
+        if not ((len(cols) == 4 and isPE) or (len(cols) == 3 and not isPE)):
+            file.close()
+            raise InvalidGroupsFileError(isPE)
+        cols[2] = os.path.abspath(os.path.join(os.path.dirname(groups_file), cols[2]))
         if len(cols) == 3:
-            cols[1] = os.path.abspath(os.path.join(os.path.dirname(groups_file), cols[1]))
-        groups.append(tuple(cols))
+            groups.append(GroupEntry_SE._make(cols))
+        if len(cols) == 4:
+            cols[3] = os.path.abspath(os.path.join(os.path.dirname(groups_file), cols[3]))
+            groups.append(GroupEntry_PE._make(cols))
     file.close()
     return groups
 
@@ -40,12 +60,38 @@ def validate_argsfiles(groups_file, ref_genome, ref_annotation):
 
 def validate_inputfiles(groups, isPE):
     for entry in groups:
-        if not ((len(entry) == 3 and isPE) or (len(entry) == 2 and not isPE)):
+        if not ((len(entry) == 4 and isPE) or (len(entry) == 3 and not isPE)):
             raise InvalidGroupsFileError(isPE)
-        if not os.path.isfile(entry[0]):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), entry[0])
-        if isPE and not os.path.isfile(entry[1]):
-            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), entry[1])
+        if not os.path.isfile(entry[2]):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), entry[2])
+        if isPE and not os.path.isfile(entry[3]):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), entry[3])
+
+
+def create_output_directory(output_path):
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    elif not os.path.isdir(output_path):
+        raise NotADirectoryError(filename=output_path)
+    os.makedirs(os.path.join(output_path, "genome_index"))
+
+
+def create_snakefile(snakefile, output_folder, ref_genome, ref_annotation, groups, isPE):
+    with open(snakefile, 'r') as sf:
+        content = sf.read()
+    content = content.replace("%%SAMPLE_NAMES%%", ", ".join([entry.name for entry in groups]))
+    if isPE:
+        content = content.replace("%%SAMPLE_PATHS%%", ", ".join(
+            [entry.name + ": [" + entry.forward + ", " + entry.reverse + "]" for entry in groups]))
+    else:
+        content = content.replace("%%SAMPLE_PATHS%%", ", ".join(
+            [entry.name + ": " + entry.file for entry in groups]))
+    content = content.replace("%%GENOME_FASTA%%", ref_genome)
+    content = content.replace("%%GENOME_INDEX%%", os.path.join(output_folder, "genome_index/genome"))
+    content = content.replace("%%GENOME_GFF%%", ref_annotation)
+    content = content.replace("%%OUTPUT_FOLDER%%", output_folder)
+    with open(os.path.join(output_folder, "snakefile"), 'w') as sf:
+        sf.write(content)
 
 
 def parse_arguments():
@@ -59,6 +105,7 @@ def parse_arguments():
     pe_se.add_argument('--pe', dest='pe', action='store_true')
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
     return parser.parse_args()
+
 
 if __name__ == '__main__':
     main()
--- dge_pipeline/tests/test_dge.py
+++ dge_pipeline/tests/test_dge.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+import tempfile
+import re
 
 from dge_pipeline import dge
 from dge_pipeline import customErrors
@@ -14,17 +16,21 @@ REF_ANNOTATION = os.path.dirname(__file__) + "/ressources/reference/listeria_mon
 
 class TestGroupsfileParser(unittest.TestCase):
     def test_parser(self):
-        groups = dge.parse_groups_file(GROUPS_FILE_PE)
+        groups = dge.parse_groups_file(GROUPS_FILE_PE, True)
 
         self.assertEqual(len(groups), 2)
-        self.assertTupleEqual(groups[0],
-                              (os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985535_R1.fastq"),
+        self.assertTupleEqual(tuple(groups[0]),
+                              ("ERR985535",
+                               "extracellular",
+                               os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985535_R1.fastq"),
                                os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985535_R2.fastq"),
-                               "extracellular"))
-        self.assertTupleEqual(groups[1],
-                              (os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985536_R1.fastq"),
+                               ))
+        self.assertTupleEqual(tuple(groups[1]),
+                              ("ERR985536",
+                               "intracellular",
+                               os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985536_R1.fastq"),
                                os.path.join(os.path.dirname(GROUPS_FILE_PE), "input_data/ERR985536_R2.fastq"),
-                               "intracellular"))
+                               ))
 
 
 class TestArgsFilesValidation(unittest.TestCase):
@@ -48,7 +54,7 @@ class TestArgsFilesValidation(unittest.TestCase):
 class TestInputFilesValidation(unittest.TestCase):
     def test_correct_cases(self):
         try:
-            groups_se = dge.parse_groups_file(GROUPS_FILE_SE)
+            groups_se = dge.parse_groups_file(GROUPS_FILE_SE, False)
             dge.validate_inputfiles(groups_se, False)
         except customErrors.InvalidGroupsFileError as err:
             self.fail(f"InvalidGroupsFileError: {err}")
@@ -57,7 +63,7 @@ class TestInputFilesValidation(unittest.TestCase):
         except IOError as err:
             self.fail(f"IOError: {err}")
         try:
-            groups_pe = dge.parse_groups_file(GROUPS_FILE_PE)
+            groups_pe = dge.parse_groups_file(GROUPS_FILE_PE, True)
             dge.validate_inputfiles(groups_pe, True)
         except customErrors.InvalidGroupsFileError as err:
             self.fail(f"InvalidGroupsFileError: {err}")
@@ -67,17 +73,26 @@ class TestInputFilesValidation(unittest.TestCase):
             self.fail(f"IOError: {err}")
 
     def test_incorrect_groups_files(self):
-        groups_se = dge.parse_groups_file(GROUPS_FILE_SE)
-        groups_pe = dge.parse_groups_file(GROUPS_FILE_PE)
-        groups_defect = dge.parse_groups_file(GROUPS_FILE_DEFECT)
+        groups_se = dge.parse_groups_file(GROUPS_FILE_SE, False)
+        groups_pe = dge.parse_groups_file(GROUPS_FILE_PE, True)
+        with self.assertRaises(customErrors.InvalidGroupsFileError):
+            dge.parse_groups_file(GROUPS_FILE_DEFECT, True)
         with self.assertRaises(customErrors.InvalidGroupsFileError):
             dge.validate_inputfiles(groups_se, True)
         with self.assertRaises(customErrors.InvalidGroupsFileError):
             dge.validate_inputfiles(groups_pe, False)
-        with self.assertRaises(customErrors.InvalidGroupsFileError):
-            dge.validate_inputfiles(groups_defect, True)
-        with self.assertRaises(customErrors.InvalidGroupsFileError):
-            dge.validate_inputfiles(groups_defect, False)
+
+
+class TestSnakefile(unittest.TestCase):
+    def test_create_snakefile(self):
+        tmp = tempfile.TemporaryDirectory()
+        dge.create_output_directory(tmp.name)
+        groups = dge.parse_groups_file(GROUPS_FILE_PE, True)
+        dge.create_snakefile(dge.SNAKEFILES["bowtie2"], tmp.name, REF_GENOME, REF_ANNOTATION, groups, True)
+        self.assertTrue(os.path.exists(os.path.join(tmp.name, "snakefile")))
+        with open(os.path.join(tmp.name, "snakefile"), 'r') as sf:
+            self.assertIsNone(re.search("%%.*%%", sf.read()))
+        tmp.cleanup()
 
 
 if __name__ == '__main__':
