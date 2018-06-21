cfe811df312089eab0c9695296a7db4101e2a17d
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-06-21 18:55:51 +0200

    add feature counts assignment plot

--- dge_pipeline/snakefiles/analyses/deseq2/lib/deseq2_analysis.R
+++ dge_pipeline/snakefiles/analyses/deseq2/lib/deseq2_analysis.R
@@ -1,6 +1,9 @@
 library("DESeq2")
 library("BiocParallel")
 library("pheatmap")
+library("ggplot2")
+library("reshape2")
+
 
 create_correlation_matrix <- function(countdata, conditiontable, output) {
     countdata.normalized.processed <- as.matrix(countdata)
@@ -13,9 +16,34 @@ create_correlation_matrix <- function(countdata, conditiontable, output) {
     dev.off()
 }
 
+
+#create_feature_counts_statistics <- function(featureCountsLog, output) {
+#    d <- read.table(featureCountsLog, header=T, row.names = 1)
+#    colnames(d) <- gsub(".bam", "", colnames(d))
+
+#    dpct <- t(t(d)/colSums(d))
+
+#    dm <- melt(t(d))
+#    dpctm <- melt(t(dpct))
+
+#    colnames(dm) <- c("Sample", "Group", "Reads")
+#    dm$Group <- factor(dm$Group, levels = rev(levels(dm$Group)[order(levels(dm$Group))]))
+
+#    pdf(output)
+#    ggplot(dm[dm$Reads > 0,], aes(x=Sample, y=Reads)) + geom_bar(aes(fill=Group), stat="identity", group=1) +
+#      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#    colnames(dpctm) <- c("Sample", "Group", "Reads")
+#    dpctm$Group <- factor(dpctm$Group, levels = rev(levels(dpctm$Group)))
+#    ggplot(dpctm[dpctm$Reads > 0,], aes(x=Sample, y=Reads)) + geom_bar(aes(fill=Group), stat="identity", group=1) +
+#      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#    dev.off()
+#}
+
 args <- commandArgs(TRUE)
 counttable_file <- args[match('--counttable', args) + 1]
 condition_file <- args[match('--conditions', args) + 1]
+feature_counts_log_file <- args[match('--featcounts-log', args) + 1]
 output_folder <- args[match('--output', args) + 1]
 threads <- args[match('--threads', args) + 1]
 register(MulticoreParam(threads))
@@ -37,6 +65,28 @@ deseq.results <- DESeq(object = deseqDataset, parallel = TRUE)
 save.image(paste(output_folder, "/deseq2.RData", sep=""))
 
 create_correlation_matrix(countdata.normalized, conditiontable, paste(output_folder, 'correlation_heatmap.pdf', sep=""))
+#create_feature_counts_statistics(feature_counts_log_file, paste(output_folder, 'count_assignment.pdf', sep=""))
+
+d <- read.table(feature_counts_log_file, header=T, row.names = 1)
+colnames(d) <- gsub(".bam", "", colnames(d))
+
+dpct <- t(t(d)/colSums(d))
+
+dm <- melt(t(d))
+dpctm <- melt(t(dpct))
+
+colnames(dm) <- c("Sample", "Group", "Reads")
+dm$Group <- factor(dm$Group, levels = rev(levels(dm$Group)[order(levels(dm$Group))]))
+
+pdf(paste(output_folder, 'count_assignment.pdf', sep=""))
+ggplot(dm[dm$Reads > 0,], aes(x=Sample, y=Reads)) + geom_bar(aes(fill=Group), stat="identity", group=1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+colnames(dpctm) <- c("Sample", "Group", "Reads")
+dpctm$Group <- factor(dpctm$Group, levels = rev(levels(dpctm$Group)))
+ggplot(dpctm[dpctm$Reads > 0,], aes(x=Sample, y=Reads)) + geom_bar(aes(fill=Group), stat="identity", group=1) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+dev.off()
 
 # Create all DESeq2 comparisons
 dir.create(paste(output_folder, "deseq2_comparisons", sep=""))
