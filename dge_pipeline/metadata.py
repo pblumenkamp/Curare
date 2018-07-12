b0167805b3437b0380bc8d6b3902dfbfde0e5c3c
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2018-07-12 14:54:23 +0200

    move from X.Y to X.Y.Z version format

--- dge_pipeline/metadata.py
+++ dge_pipeline/metadata.py
@@ -1,4 +1,4 @@
 __program_name__ = "Differential gene expression pipeline generator"
-__version__ = "0.1"
+__version__ = "0.1.0"
 __author__ = "Patrick Blumenkamp"
 __author_email__ = "patrick.blumenkamp@computational.bio.uni-giessen.de"
