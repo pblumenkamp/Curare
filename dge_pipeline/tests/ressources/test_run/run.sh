dcccbe8f25517c6ebdbf957dd1204bc27ddeb2d3
Patrick Blumenkamp <patrick.blumenkamp@computational.uni-giessen.de>
2019-01-17 17:04:36 +0100

    add test cases for bwa-mem

--- dge_pipeline/tests/ressources/test_run/run.sh
+++ dge_pipeline/tests/ressources/test_run/run.sh
@@ -12,6 +12,10 @@ if [ $? -ne 0 ]; then FAILEDRUNS+=("Paired_end"); fi
 bash "`dirname $SCRIPTPATH`/zipped_run/run.sh"
 if [ $? -ne 0 ]; then FAILEDRUNS+=("Zipped_run"); fi
 
+bash "`dirname $SCRIPTPATH`/single_end_run_bwamem/run.sh"
+if [ $? -ne 0 ]; then FAILEDRUNS+=("Single_end_bwa_mem"); fi
+
+
 echo "\n\n"
 echo "Aborted runs:"
 for run in $FAILEDRUNS; do
