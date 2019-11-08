8df1a3f4c799a47f53b339790c2464c096f5b64d
Christian Blume <christian.blume@bioinfsys.uni-giessen.de>
2019-11-08 12:14:29 +0100

    Added rule to bowtie2_pe module. Generates its own report data used by generate_report.py.

---
+++ dge_pipeline/snakefiles/mapping/bowtie2/lib/generate_report_data.py
@@ -0,0 +1,28 @@
+import json
+import sys
+import pandas as pd
+
+from os.path import abspath
+from pathlib import Path
+
+
+def parse_mapping_stats_to_json(src_dir: Path, target_dir: Path):
+    tsv_df = pd.read_csv(src_dir / 'stats' / 'mapping_stats.tsv', sep="\t")
+    json_obj = json.loads(tsv_df.to_json(orient='records'))
+    with open(target_dir / 'data' / 'mapping_stats.json', 'w') as f:
+        json.dump(json_obj, f, indent=4)
+
+
+def generate_report_data(src_dir: Path, target_dir: Path):
+    parse_mapping_stats_to_json(src_dir, target_dir)
+
+
+def main():
+    mapping_dir = Path(abspath(sys.argv[1]))
+    report_dir = Path(abspath(sys.argv[2]))
+
+    generate_report_data(mapping_dir, report_dir)
+
+
+if __name__ == '__main__':
+    main()
