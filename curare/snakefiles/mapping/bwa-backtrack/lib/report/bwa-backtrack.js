78bde18ed0cc03df2bf13d78bbfe6da91930b319
Patrick Blumenkamp <patrick.blumenkamp@computational.bio.uni-giessen.de>
2021-06-24 12:09:20 +0200

    add "settings" block to report (bwa-backtrack); allow more detailed setting of parameters

--- curare/snakefiles/mapping/bwa-backtrack/lib/report/bwa-backtrack.js
+++ curare/snakefiles/mapping/bwa-backtrack/lib/report/bwa-backtrack.js
@@ -5,9 +5,46 @@ new Vue({
     data: {
         bwa_stats: Curare.bwa_backtrack.stats,
         paired_end: Curare.bwa_backtrack.paired_end,
+        bwa_settings: Curare.bwa_backtrack.settings,
         stats_percent_header_paired_end: ["", ""],
-        stats_in_percent: []
+        stats_in_percent: [],
+        bwa_settings_columns: [
+            {
+                field: 'key',
+                label: 'Key',
+                visible: true
+            },
+            {
+                field: 'value',
+                label: 'Value',
+                visible: true
+            }
+        ]
     },
-    computed: {},
+    computed: {
+        bwa_settings_data: function() {
+            let vue = this
+            let samse_sampe_key = (vue.paired_end) ? "additional_sampe_parameters" : "additional_samse_parameters"  
+            let settings = [
+                {
+                    key: "BWA Version",
+                    value: vue.bwa_settings["bwa_version"] 
+                },
+                {
+                    key: "Sequencing Strategy",
+                    value: (vue.paired_end) ? "Paired-End" : "Single-End"
+                },
+                {
+                    key: "Additional BWA ALN Parameters",
+                    value: (vue.bwa_settings["additional_aln_parameters"]) ? '"' + vue.bwa_settings["additional_aln_parameters"] + '"' : "-"
+                },
+                {
+                    key: (vue.paired_end) ? "Additional BWA SAMPE Parameters" : "Additional BWA SAMSE Parameters",
+                    value: (vue.bwa_settings[samse_sampe_key]) ? '"' + vue.bwa_settings[samse_sampe_key] + '"' : "-"
+                },
+            ]
+            return settings
+        }
+    },
     methods: {}
 });
