23c68785c57c8c62ae27182c1ebc7f7f077ffb26
Patrick Blumenkamp <patrick.blumenkamp@bioinfsys.uni-giessen.de>
2021-11-10 17:46:22 +0100

    add full support for gzipped fastq files

--- tests/resources/test_run/paired_end_run_trim_galore/run_with_conda.sh
+++ tests/resources/test_run/paired_end_run_trim_galore/run_with_conda.sh
@@ -4,4 +4,4 @@ SCRIPTPATH=`readlink -f $0`
 SCRIPTDIR=`dirname $SCRIPTPATH`
 
 rm -r ${SCRIPTDIR}/output_dir
-python3 ${SCRIPTDIR}/../../../../curare/curare.py --samples ${SCRIPTDIR}/samples.tsv --pipeline ${SCRIPTDIR}/pipeline.yml --output ${SCRIPTDIR}/output_dir -t 4 --use-conda --conda-prefix ../../envs/
+python3 ${SCRIPTDIR}/../../../../curare/curare.py --samples ${SCRIPTDIR}/samples.tsv --pipeline ${SCRIPTDIR}/pipeline.yml --output ${SCRIPTDIR}/output_dir -t 4 --use-conda --conda-prefix ../../envs/ --latency-wait 10
