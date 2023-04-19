dc3ab690735a5c2d4f5c94b82d3b9fce7316969d
Patrick Blumenkamp <patrick.blumenkamp@bioinfsys.uni-giessen.de>
2023-04-19 17:14:22 +0200

    fix GFF/GTF bug in TSV to XLSX converter

--- curare/snakefiles/analysis/dge_analysis/lib/deseq2_summary_tsv_to_xlsx.py
+++ curare/snakefiles/analysis/dge_analysis/lib/deseq2_summary_tsv_to_xlsx.py
@@ -83,9 +83,8 @@ def main():
     else:
         gff = open(gff_file, "r")
         gtf_or_gff = "gtf" if gff_file.endswith("gtf") else "gff"
-    if gtf_or_gff == "gtf":
-        attribute_separator: str = ';' if gtf_or_gff == "gtf" else ';'
-        key_value_separator: str = ' ' if gtf_or_gff == "gtf" else '='
+    attribute_separator: str = ';' if gtf_or_gff == "gtf" else ';'
+    key_value_separator: str = ' ' if gtf_or_gff == "gtf" else '='
     for line in gff:
         try:
             if line.startswith("#"):
@@ -154,82 +153,78 @@ def main():
 
     # convert to pandas data frame, so pandas ExcelWriter is usable
     df = pd.DataFrame(list_data[1:], columns=list_data[0])
-    writer = pd.ExcelWriter(outfile,
-                            engine='xlsxwriter',
-                            engine_kwargs={'options': {'strings_to_numbers': True}}
-                            )
-    df.to_excel(writer, sheet_name=sheet_name, index=False)
+    with pd.ExcelWriter(outfile, engine='xlsxwriter', engine_kwargs={'options': {'strings_to_numbers': True}}) as writer:
+        df.to_excel(writer, sheet_name=sheet_name, index=False)
 
-    columns -= 1  # because of removing of first column
-    workbook = writer.book
-    worksheet = writer.sheets[sheet_name]
+        columns -= 1  # because of removing of first column
+        workbook = writer.book
+        worksheet = writer.sheets[sheet_name]
 
-    for i, width in enumerate(get_col_widths(df)):
-        worksheet.set_column(i, i, width + 5)
+        for i, width in enumerate(get_col_widths(df)):
+            worksheet.set_column(i, i, width + 5)
 
-    p_value_format = {'type': '3_color_scale',
-                      'min_color': "#7FFFAD",
-                      'mid_color': "#EDE97E",
-                      'max_color': "#FF0000",
-                      'mid_type': 'num',
-                      'min_type': 'num',
-                      'max_type': 'num',
-                      'min_value': 0.001,
-                      'max_value': 1,
-                      'mid_value': 0.01}
+        p_value_format = {'type': '3_color_scale',
+                        'min_color': "#7FFFAD",
+                        'mid_color': "#EDE97E",
+                        'max_color': "#FF0000",
+                        'mid_type': 'num',
+                        'min_type': 'num',
+                        'max_type': 'num',
+                        'min_value': 0.001,
+                        'max_value': 1,
+                        'mid_value': 0.01}
 
-    log_fold_format = {'type': '3_color_scale',
-                       'min_color': "#7FFFAD",
-                       'mid_color': "#FFFFFF",
-                       'mid_type': 'num',
-                       'mid_value': 0,
-                       'max_color': "#FF0000"}
+        log_fold_format = {'type': '3_color_scale',
+                        'min_color': "#7FFFAD",
+                        'mid_color': "#FFFFFF",
+                        'mid_type': 'num',
+                        'mid_value': 0,
+                        'max_color': "#FF0000"}
 
-    count_format = {'type': '3_color_scale',
-                    'min_color': "#FFFFFF",
-                    'mid_color': "#EDE97E",
-                    'max_color': "#FF0000",
-                    'mid_type': 'num',
-                    'max_type': 'num',
-                    'min_type': 'num',
-                    'min_value': 0,
-                    'mid_value': 100,
-                    'max_value': 1000}
+        count_format = {'type': '3_color_scale',
+                        'min_color': "#FFFFFF",
+                        'mid_color': "#EDE97E",
+                        'max_color': "#FF0000",
+                        'mid_type': 'num',
+                        'max_type': 'num',
+                        'min_type': 'num',
+                        'min_value': 0,
+                        'mid_value': 100,
+                        'max_value': 1000}
 
-    # conditional formatting
-    worksheet.conditional_format(1, 4 + len(wanted_gff_attributes), rows - 1, 2 * number_of_conditions + len(wanted_gff_attributes) + 3,
-                                 p_value_format)
-    worksheet.conditional_format(1, 4 + len(wanted_gff_attributes) + 2 * number_of_conditions, rows - 1,
-                                 3 * number_of_conditions + len(wanted_gff_attributes) + 3, log_fold_format)
-    worksheet.conditional_format(1, 4 + len(wanted_gff_attributes) + 3 * number_of_conditions, rows - 1, columns - 1, count_format)
+        # conditional formatting
+        worksheet.conditional_format(1, 4 + len(wanted_gff_attributes), rows - 1, 2 * number_of_conditions + len(wanted_gff_attributes) + 3,
+                                    p_value_format)
+        worksheet.conditional_format(1, 4 + len(wanted_gff_attributes) + 2 * number_of_conditions, rows - 1,
+                                    3 * number_of_conditions + len(wanted_gff_attributes) + 3, log_fold_format)
+        worksheet.conditional_format(1, 4 + len(wanted_gff_attributes) + 3 * number_of_conditions, rows - 1, columns - 1, count_format)
 
-    # freeze first row and column
-    worksheet.freeze_panes(1, 1)
+        # freeze first row and column
+        worksheet.freeze_panes(1, 1)
 
-    # autofilter for each column
-    worksheet.autofilter(0, 0, rows - 1, columns)
+        # autofilter for each column
+        worksheet.autofilter(0, 0, rows - 1, columns)
 
-    # legend sheet
-    legend_sheet = workbook.add_worksheet("legend")
-    legend_sheet.write(0, 0, "p-values")
-    legend_sheet.write(0, 1, "lfc")
-    legend_sheet.write(0, 2, "counts")
+        # legend sheet
+        legend_sheet = workbook.add_worksheet("legend")
+        legend_sheet.write(0, 0, "p-values")
+        legend_sheet.write(0, 1, "lfc")
+        legend_sheet.write(0, 2, "counts")
 
-    legend_sheet.write(1, 0, 0.001)
-    legend_sheet.write(2, 0, 0.01)
-    legend_sheet.write(3, 0, 1)
-    legend_sheet.conditional_format(1, 0, 3, 0, p_value_format)
+        legend_sheet.write(1, 0, 0.001)
+        legend_sheet.write(2, 0, 0.01)
+        legend_sheet.write(3, 0, 1)
+        legend_sheet.conditional_format(1, 0, 3, 0, p_value_format)
 
-    legend_sheet.write(1, 1, "min", workbook.add_format({'bg_color': "#7FFFAD"}))
-    legend_sheet.write(2, 1, 0, workbook.add_format({'bg_color': "#FFFFFF"}))
-    legend_sheet.write(3, 1, "max", workbook.add_format({'bg_color': "#FF0000"}))
+        legend_sheet.write(1, 1, "min", workbook.add_format({'bg_color': "#7FFFAD"}))
+        legend_sheet.write(2, 1, 0, workbook.add_format({'bg_color': "#FFFFFF"}))
+        legend_sheet.write(3, 1, "max", workbook.add_format({'bg_color': "#FF0000"}))
 
-    legend_sheet.write(1, 2, 0)
-    legend_sheet.write(2, 2, 100)
-    legend_sheet.write(3, 2, 1000)
-    legend_sheet.conditional_format(1, 2, 3, 2, count_format)
-
-    writer.save()
+        legend_sheet.write(1, 2, 0)
+        legend_sheet.write(2, 2, 100)
+        legend_sheet.write(3, 2, 1000)
+        legend_sheet.conditional_format(1, 2, 3, 2, count_format)
+        
 
 def parse_attributes(attributes: str, attribute_separator: str=';', key_value_separator: str='=', quotes: str='"') -> Dict[str,str]:
     state: str = 'first_letter_key'  # first_letter_key, key, value, quotes
