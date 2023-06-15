80d4c585c061ac8a6d7493a7640fe5216d99086f
Patrick Blumenkamp <patrick.blumenkamp@bioinfsys.uni-giessen.de>
2023-06-15 14:14:51 +0200

    add customization settings to fastp

--- tests/resources/test_run/paired_end_run_fastp/run.sh
+++ tests/resources/test_run/paired_end_run_fastp/run.sh
@@ -4,4 +4,4 @@ SCRIPTPATH=`readlink -f $0`
 SCRIPTDIR=`dirname $SCRIPTPATH`
 
 #rm -r ${SCRIPTDIR}/output_dir
-${SCRIPTDIR}/../../../../bin/curare --samples ${SCRIPTDIR}/samples.tsv --pipeline ${SCRIPTDIR}/pipeline.yml --output ${SCRIPTDIR}/output_dir -t 4
+${SCRIPTDIR}/../../../../bin/curare --samples ${SCRIPTDIR}/samples.tsv --pipeline ${SCRIPTDIR}/pipeline.yaml --output ${SCRIPTDIR}/output_dir -t 4
